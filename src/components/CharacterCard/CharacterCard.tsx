a8e8e991020fb0b013bb3a22ceff162068d9bbe7
Joseph <jm.seph28@gmail.com>
2025-01-24 00:26:32 +0800

    unique id

--- src/components/CharacterCard/CharacterCard.tsx
+++ src/components/CharacterCard/CharacterCard.tsx
@@ -10,7 +10,7 @@ interface CharacterCardProps {
 
 const CharacterCard: React.FC<CharacterCardProps> = ({ character, navigation }) => {
   return (
-    <View style={styles.card}>
+    <View style={styles.card} key={character.id}>
       <Pressable onPress={() => navigation.navigate('CharacterDetails', { character })}>
         <Image source={{ uri: character.image }} style={styles.image} />
       </Pressable>
